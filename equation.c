f8689bf5cf143036ab9ff6c33243b26202301a93
drawchess <1603331894@qq.comg>
2018-10-30 20:45:36 +0800

    helloworld add commit

--- equation.c
+++ equation.c
@@ -2,3 +2,24 @@
 //ax + by = c
 //dx + ey = f
 //注意输入/输出，数据类型
+#include <stdio.h>
+int main (){
+	double a,b,c,d,e,f,x,y;
+	printf("a=");
+	scanf("%lf",&a);
+	printf("b=");
+	scanf("%lf",&b);
+	printf("c=");
+	scanf("%lf",&c);
+	printf("d=");
+	scanf("%lf",&d);
+	printf("e=");
+	scanf("%lf",&e);
+	printf("f=");
+	scanf("%lf",&f);
+	x=(c*e-b*f )/(e*a-b*d);
+	y=(c*d-a*f)/(b*d-a*e);
+	printf("x=%lf",x);
+	printf("y=%lf",y);
+}
+ 
--- equation1.c
+++ equation1.c
@@ -1,3 +1,18 @@
-//求解一元二次方程的根
-//查阅sqrt()函数
-//注意有没有解
+
+#include <stdio.h>
+#include <math.h>
+int main(){
+	double a,b,c,x1,x2,delta;
+	printf("a=");
+	scanf("%lf",&a);
+	printf("b=");
+	scanf("%lf",&b);
+	printf("c=");
+	scanf("%lf",&c);
+	delta=b*b-4*a*c;
+	if(delta>=0){
+		x1=(-b+sqrt(delta))/2*a,x2=(-b-sqrt(delta))/2*a;
+		printf("x1=%lf x2=%lf",x1,x2);
+	} 
+	return 0;
+}
